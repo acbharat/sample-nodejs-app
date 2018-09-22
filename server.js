ed97798a8827886cac1bf0cb64b8677f4cd91655
acbharat <acbharat@gmail.com>
2018-09-22 08:14:39 -0400

    change server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world from a PPCRS Node.js app!')
+    res.send('Happy Coding -team PPCRS Node.js app!')
 })
 app.listen(6000, () => {
     console.log('Server is up on 6000')
